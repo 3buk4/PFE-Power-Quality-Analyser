15a2b21d7caeb84a0d5cb371ac047f1f3c83e3f8
Jude <victoryjude003@gmail.com>
2026-04-12 15:47:40 +0100

    Defined Waveform sample and math prototypes

--- Waveform.h
+++ Waveform.h
@@ -5,4 +5,15 @@
 #ifndef PFE_POWER_QUALITY_ANALYSER_WAVEFORM_H
 #define PFE_POWER_QUALITY_ANALYSER_WAVEFORM_H
 
+typedef struct {
+    double timestamp; //Column 1: Time Stamp
+    double phase_a; //Column 2: Voltage Phase A
+    double phase_b; //Column 3: Voltage Phase B
+    double phase_c; //Column 4: Voltage Phase c
+    double current; //Column 5: Line Current
+    double frequency; //Column 6: Frequency
+    double power_factor; //Column 7: Power Factor
+    double thd; //Column 8: thd_Percent (Total Harmonic Distortion)
+} Waveformsample;
+
 #endif //PFE_POWER_QUALITY_ANALYSER_WAVEFORM_H
