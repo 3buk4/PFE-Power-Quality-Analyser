c2ffdbf04cd2a850d9db03c8444f152a026632c8
Jude <victoryjude003@gmail.com>
2026-04-14 19:55:43 +0100

    Implement RMS calculation Logic in Waveform moudle

--- Waveform.c
+++ Waveform.c
@@ -1,3 +1,30 @@
 //
 // Created by PC on 4/12/2026.
 //
+
+#include "Waveform.h"
+#include <math.h>
+#include <stdio.h>
+
+double calculate_rms(Waveformsample *data_array, int count, char phase_char) {
+
+    if (count <= 0 || data_array == NULL) return 0; //if file is empty exit immediately
+
+    double sum_sq = 0.0; //variable for v^2
+    for (int i = 0; i < count; i++) {//loops through every row
+        double val = 0.0;// Variable to store the specific voltage
+
+
+        if (phase_char == 'A')  val = data_array[i].phase_A_voltage; //checks phase char and grabs data
+        else if (phase_char == 'B')  val = data_array[i].phase_B_voltage;
+        else if (phase_char == 'C')  val = data_array[i].phase_C_voltage;
+
+        sum_sq += val * val;
+
+        double rms = sqrt(sum_sq / count);
+        return rms;
+
+    }
+
+
+}
